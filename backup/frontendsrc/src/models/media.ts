4f86792fa8f866ad8b3061a2f803bff7b0203758
Jordan Coil <albert.jordan.coil@gmail.com>
2023-02-23 12:31:52 -0800

    ignoring backup files

--- backup/frontendsrc/src/models/media.ts
+++
@@ -1,9 +0,0 @@
-export interface Media {
-    _id: string,
-    title: string,
-    description?: string,
-    subtitleFile?: string,
-    imgFile?: string,
-    createdAt: string,
-    updatedAt: string,
-}
